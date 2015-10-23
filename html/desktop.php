c8540fe2cf469a4931e5ff97c6ab522b071809c0
admin <master@salvo.ru>
2015-10-23 16:13:12 +0300

    Добавлен "Рабочий стол" desktop.php

---
+++ html/desktop.php
@@ -0,0 +1,159 @@
+<?
+	include ('inc/_head.php');
+?>
+<body>
+
+<?
+	include ('inc/_header.php');
+?>
+
+<!-- MiddleBlock -->
+<div class="middle-block">
+	<div class="wrapper">
+
+		<!-- BreadcrumbsBlock -->
+		<ul class="breadcrumbs-block">
+			<!--
+				<li><a href="#">Рабочий стол</a></li>
+				<li><a href="#">Организация</a></li>
+				<li><a href="#">Общие сведения</a></li>
+			 -->
+		</ul>
+		<!-- /BreadcrumbsBlock -->
+
+		<!-- LeftColumn -->
+		<div class="left-column col-xs-3">
+
+			<!-- StepsMenuBlock -->
+			<ul class="steps-menu-block">
+				<li><a href="#">Шаг 1</a></li>
+				<li><a href="#">Шаг 2</a></li>
+				<li><a href="#">Шаг 3</a></li>
+				<li><a href="#">Шаг 4</a></li>
+				<li><a href="#">Шаг 5</a></li>
+				<li><a href="#">Шаг 6</a></li>
+			</ul>
+			<!-- /StepsMenuBlock -->
+
+		</div>
+		<!-- /LeftColumn -->
+
+		<!-- RightColumn -->
+		<div class="right-column col-xs-9">
+
+			<!-- StepsContentBlock -->
+			<ul class="steps-content-block">
+
+				<!-- Step1 -->
+				<li>
+					Чтобы система разработала необходимый документ, Вам совершенно необязательно обладать знаниями в области защиты персональных данных <br/>
+					<br/>
+					Вы просто заполняете <a href="#">анкету</a>, производя ввод сведений об организации, информационной системе и мероприятиях по ее защите в отдельные поля. <br/>
+					<br/>
+					Чтобы процесс внесения информации был еще более удобным и быстрым, анкетирование сопровождается подробными разъяснениям, примечаниями, а также подсказками в форме всплывающих списков для выбора, подходящего Вам варианта ответа. <br/>
+					<br/>
+					После того, как Вы заполняете анкету, на основе введенных данных система формирует требуемый документ.
+
+					<div class="clearfix"></div>
+					<br/><br/><br/>
+					<a href="#" class="button button-prev">Назад</a>
+					<a href="#" class="button button-next pull-right">Далее</a>
+					<div class="clearfix"></div>
+					<br/><br/>
+					<div class="center-text"><a href="#" class="button button-red">Заполнить анкету</a></div>
+				</li>
+				<!-- /Step1 -->
+
+				<!-- Step2 -->
+				<li>
+					Далее Вам остается только скачать документ, По форме и содержанию он полностью будет соответствовать требованиям делопроизводства и законодательства в области защиты персональных данных. <br/>
+					<br/>
+					Для Вашего удобства к документу прилагается пояснение по его назначению, дальнейших действий с ним и сроков его актуальности.
+
+					<div class="clearfix"></div>
+					<br/><br/><br/>
+					<a href="#" class="button button-prev">Назад</a>
+					<a href="#" class="button button-next pull-right">Далее</a>
+					<div class="clearfix"></div>
+					<br/><br/>
+					<div class="center-text"><a href="#" class="button button-red">Документы</a></div>
+				</li>
+				<!-- /Step2 -->
+
+				<!-- Step3 -->
+				<li>
+					Если в ходе работы с созданием документа у Вас появятся затруднения или вопросы, а также если Вам нужна консультация по какому-либо аспекту в сфере защиты ПД, Вы в любое время суток можете воспользоваться формой обратной связи или заказать обратный звонок, и наш эксперт по информационной безопасности в ближайшее время свяжется с Вами.
+
+					<div class="clearfix"></div>
+					<br/><br/><br/>
+					<a href="#" class="button button-prev">Назад</a>
+					<a href="#" class="button button-next pull-right">Далее</a>
+					<div class="clearfix"></div>
+					<br/><br/>
+					<div class="center-text"><a href="#" class="button button-red">Вопросы к эксперту</a></div>
+				</li>
+				<!-- /Step3 -->
+
+				<!-- Step4 -->
+				<li>
+					Чтобы подлинность Ваших документов никогда не вызывала сомнений, их содержание необходимо синхронизировать с последними версиями шаблонов Следуйте этой рекомендации не реже, чем раз в 3 месяца. <br/>
+					<br/>
+					Мы тщательно отслеживаем все изменения в законодательстве в области защиты ПД и будем сообщать Вам о них посредством нашей системы.<br/>
+					<br/>
+					Если Вашему документу потребуется обновление, система запросит у вас новые данные и создаст актуальную версию.
+
+					<div class="clearfix"></div>
+					<br/><br/><br/>
+					<a href="#" class="button button-prev">Назад</a>
+					<a href="#" class="button button-next pull-right">Далее</a>
+					<div class="clearfix"></div>
+					<br/><br/>
+					<div class="center-text"><a href="#" class="button button-red">Перейти к настройкам</a></div>
+				</li>
+				<!-- /Step4 -->
+
+				<!-- Step5 -->
+				<li>
+					Нашей компанией предусмотрен выезд эксперта на объект к клиенту и удаленная техническая поддержка, если Вам требуется квалифицированная помощь в организации защиты персональных данных или подготовке к проверке регуляторами.
+
+					<div class="clearfix"></div>
+					<br/><br/><br/>
+					<a href="#" class="button button-prev">Назад</a>
+					<a href="#" class="button button-next pull-right">Далее</a>
+					<div class="clearfix"></div>
+					<br/><br/>
+					<div class="center-text"><a href="#" class="button button-red">Заказать выезд эксперта на объект </a></div>
+				</li>
+				<!-- /Step5 -->
+
+				<!-- Step6 -->
+				<li>
+					Мы готовы предоставить Вам услуги по подбору технических средств защиты информации Для получения подробной информации по этому поводу сообщите нам об этом с помощью формы обратной связи, закажите обратный звонок или позвоните по телефонам.
+
+					<div class="clearfix"></div>
+					<br/><br/><br/>
+					<a href="#" class="button button-prev">Назад</a>
+					<a href="#" class="button button-next pull-right">Далее</a>
+					<div class="clearfix"></div>
+					<br/><br/>
+					<div class="center-text"><a href="#" class="button button-red">Подбор технических средств защиты </a></div>
+				</li>
+				<!-- /Step6 -->
+
+			</ul>
+			<!-- /StepsContentBlock -->
+
+		</div>
+		<!-- /RightColumn -->
+
+	</div>
+</div>
+<!-- /MiddleBlock -->
+
+<?
+	include ('inc/_footer.php');
+	include ('inc/_windows.php');
+?>
+
+</body>
+</html>
--- html/js/index.js
+++ html/js/index.js
@@ -23,66 +23,106 @@
 		});
 		/** /pop-up-menu **/
 
+
+		/** StepsMenuBlock **/
+		$('.steps-menu-block li').each(function (i) {
+			var $this = $(this);
+			$this.find('a').on('click', function () {
+				$('.steps-menu-block li').removeClass('active');
+				$('.steps-menu-block li').eq(i).addClass('active');
+				$('.steps-content-block li').hide().eq(i).show();
+				return false;
+			});
+		});
+		$('.steps-content-block li').each(function(i) {
+			var $this = $(this);
+			var prevButton = $this.find('.button-prev');
+			var nextButton = $this.find('.button-next');
+			if (i == 0) {
+				prevButton.hide();
+			}
+			else {
+				prevButton.on('click', function() {
+					$('.steps-menu-block li').eq(i - 1).find('a').trigger('click');
+					return false;
+				});
+			}
+			if (i == $('.steps-menu-block li').length - 1) {
+				nextButton.hide();
+			}
+			else {
+				nextButton.on('click', function() {
+					$('.steps-menu-block li').eq(i + 1).find('a').trigger('click');
+					return false;
+				});
+			}
+		});
+		$('.steps-content-block .button-prev, .steps-content-block .button-next').on('click', function() {
+
+		});
+		$('.steps-menu-block li').eq(0).find('a').trigger('click');
+		/** /StepsMenuBlock **/
+
 		/** body background **/
 		/*
-		$(window).resize(function () {
-			var backgroundPosition = 'top center';
-			var winWidth = $(window).width();
-			var bgImageWidth = 1020; // ширина контентной области (ширина body)
-			if (winWidth < bgImageWidth) {
-				backgroundPosition = '-450px 0'; // левый край контентной области (= - (ширина_фона - bgImageWidth) / 2)
-			}
-			$('body').css({'backgroundPosition': backgroundPosition})
-		});
-		$(window).trigger('resize');
-		*/
+		 $(window).resize(function () {
+		 var backgroundPosition = 'top center';
+		 var winWidth = $(window).width();
+		 var bgImageWidth = 1020; // ширина контентной области (ширина body)
+		 if (winWidth < bgImageWidth) {
+		 backgroundPosition = '-450px 0'; // левый край контентной области (= - (ширина_фона - bgImageWidth) / 2)
+		 }
+		 $('body').css({'backgroundPosition': backgroundPosition})
+		 });
+		 $(window).trigger('resize');
+		 */
 		/** /body background **/
 
 
 		/** WindowForm **/
 		/*
-		$('.content-button').on('click', function () {
-			var $this = $(this);
-			if ($this.next('.button-form-content').length) {
-				var html = $this.next('.button-form-content').html()
-			}
-			else if ($this.attr('rel')) {
-				var win = $($this.attr('rel'));
-				//win.find('form input[name=formName]').val('Заказ подробностей - интересует: ' + $this.attr('product') + '');
-				win.find('form .title').eq(0).html('<!-- Вас интересует:<br />--><b>' + $this.attr('product') + '</b>');
-				var html = win.html();
-			}
-			$('#result-content').html(html);
-			$('.result-win').removeClass('answer');
-			$('.mask').show();
-			$('.mask.div').screenCenterAlign();
-			initSubmitForm();
-			return false;
-		});
-		$(this).keydown(function (eventObject) {
-			if (eventObject.which == 27) {
-				$('.mask').hide();
-			}
-		});
-		$('.mask.bg').on('click', function (eventObject) {
-			$('.mask').hide();
-		});
-		*/
+		 $('.content-button').on('click', function () {
+		 var $this = $(this);
+		 if ($this.next('.button-form-content').length) {
+		 var html = $this.next('.button-form-content').html()
+		 }
+		 else if ($this.attr('rel')) {
+		 var win = $($this.attr('rel'));
+		 //win.find('form input[name=formName]').val('Заказ подробностей - интересует: ' + $this.attr('product') + '');
+		 win.find('form .title').eq(0).html('<!-- Вас интересует:<br />--><b>' + $this.attr('product') + '</b>');
+		 var html = win.html();
+		 }
+		 $('#result-content').html(html);
+		 $('.result-win').removeClass('answer');
+		 $('.mask').show();
+		 $('.mask.div').screenCenterAlign();
+		 initSubmitForm();
+		 return false;
+		 });
+		 $(this).keydown(function (eventObject) {
+		 if (eventObject.which == 27) {
+		 $('.mask').hide();
+		 }
+		 });
+		 $('.mask.bg').on('click', function (eventObject) {
+		 $('.mask').hide();
+		 });
+		 */
 		/** /WindowForm **/
 
 		/** windows **/
 		/*
-		$('.mask.div').append('<a href="#" title="Закрыть" class="close-button" onclick="$(\'.mask\').hide(0); return false;"></a>')
-		$('.mask.bg').css({background: '#000', opacity: .6});
-		$('#win-block').appendTo('body');
-		centerMask();
-		$(window).resize(function () {
-			centerMask();
-		}).scroll(function(){
-			centerMask();
-		})
-		//$('.mask.win1').show(0);
-		*/
+		 $('.mask.div').append('<a href="#" title="Закрыть" class="close-button" onclick="$(\'.mask\').hide(0); return false;"></a>')
+		 $('.mask.bg').css({background: '#000', opacity: .6});
+		 $('#win-block').appendTo('body');
+		 centerMask();
+		 $(window).resize(function () {
+		 centerMask();
+		 }).scroll(function(){
+		 centerMask();
+		 })
+		 //$('.mask.win1').show(0);
+		 */
 		/** /windows **/
 
 		/** SubmitForms **/
@@ -138,78 +178,78 @@
 
 		/** search autocomplete **/
 		/*
-		initAutocomplete();
-		*/
+		 initAutocomplete();
+		 */
 		/** /search autocomplete **/
 	});
 
-/*
-// search autocomplete
+	/*
+	 // search autocomplete
 
-//-// catcomplete-search-widget
-	$.widget("custom.catcomplete", $.ui.autocomplete, {
-		_create: function () {
-			this._super();
-			this.widget().menu("option", "items", "> :not(.ui-autocomplete-category)");
-		},
-		_renderMenu: function (ul, items) {
-			var that = this,
-				currentCategory = "";
-			$.each(items, function (index, item) {
-				var li;
-				if (item.category != currentCategory) {
-					ul.append("<li class='ui-autocomplete-category'>" + item.category + "</li>");
-					currentCategory = item.category;
-				}
-				li = that._renderItemData(ul, item);
-				if (item.category) {
-					li.attr({
-						"aria-label": item.category + " : " + item.label,
-						"class": (item.className ? 'icon-search icon-' + item.className : '')
-					});
-				}
-			});
-		}
-	});
-//-// /catcomplete-search-widget
+	 //-// catcomplete-search-widget
+	 $.widget("custom.catcomplete", $.ui.autocomplete, {
+	 _create: function () {
+	 this._super();
+	 this.widget().menu("option", "items", "> :not(.ui-autocomplete-category)");
+	 },
+	 _renderMenu: function (ul, items) {
+	 var that = this,
+	 currentCategory = "";
+	 $.each(items, function (index, item) {
+	 var li;
+	 if (item.category != currentCategory) {
+	 ul.append("<li class='ui-autocomplete-category'>" + item.category + "</li>");
+	 currentCategory = item.category;
+	 }
+	 li = that._renderItemData(ul, item);
+	 if (item.category) {
+	 li.attr({
+	 "aria-label": item.category + " : " + item.label,
+	 "class": (item.className ? 'icon-search icon-' + item.className : '')
+	 });
+	 }
+	 });
+	 }
+	 });
+	 //-// /catcomplete-search-widget
 
-	function initAutocomplete() {
-		$(".quick-search").each(function () {
-			var $this = $(this);
-			var author = ($this.hasClass('only-author') ? true : false); // поиск только в авторах
-			var ar = {
-				delay: 300,
-				source: function (request, response) {
-					// организуем кроссдоменный запрос
-					$.ajax({
-						url: SITE_TEMPLATE_PATH + "/js/ajax/quick-search/?quick-search=y" + (author ? '&only-author=y' : ""),
-						dataType: "json",
-						// параметры запроса, передаваемые на сервер (последний - подстрока для поиска):
-						data: {
-							term: request.term
-						},
-						// обработка успешного выполнения запроса
-						success: function (data) {
-							// приведем полученные данные к необходимому формату и передадим в предоставленную функцию response
-							response(data);
-						}
-					});
-				},
-				minLength: 1
-			};
-			if (author) {
-				ar.position = {
-					my: "right top",
-					at: "right bottom",
-					collision: "none"
-				};
-			}
-			$this.catcomplete(ar);
-		});
-	}
+	 function initAutocomplete() {
+	 $(".quick-search").each(function () {
+	 var $this = $(this);
+	 var author = ($this.hasClass('only-author') ? true : false); // поиск только в авторах
+	 var ar = {
+	 delay: 300,
+	 source: function (request, response) {
+	 // организуем кроссдоменный запрос
+	 $.ajax({
+	 url: SITE_TEMPLATE_PATH + "/js/ajax/quick-search/?quick-search=y" + (author ? '&only-author=y' : ""),
+	 dataType: "json",
+	 // параметры запроса, передаваемые на сервер (последний - подстрока для поиска):
+	 data: {
+	 term: request.term
+	 },
+	 // обработка успешного выполнения запроса
+	 success: function (data) {
+	 // приведем полученные данные к необходимому формату и передадим в предоставленную функцию response
+	 response(data);
+	 }
+	 });
+	 },
+	 minLength: 1
+	 };
+	 if (author) {
+	 ar.position = {
+	 my: "right top",
+	 at: "right bottom",
+	 collision: "none"
+	 };
+	 }
+	 $this.catcomplete(ar);
+	 });
+	 }
 
-// /search autocomplete
-*/
+	 // /search autocomplete
+	 */
 
 })(jQuery);
 
@@ -241,38 +281,38 @@ function centerMask() {
 
 /** Функция, аналогичная php функции inArray **/
 /*
-inArray = Array.prototype.indexOf ?
-	function (arr, val) {
-		return arr.indexOf(val) != -1
-	} :
-	function (arr, val) {
-		var i = arr.length;
-		while (i--) {
-			if (arr[i] === val) return true
-		}
-		return false
-	};
-	*/
+ inArray = Array.prototype.indexOf ?
+ function (arr, val) {
+ return arr.indexOf(val) != -1
+ } :
+ function (arr, val) {
+ var i = arr.length;
+ while (i--) {
+ if (arr[i] === val) return true
+ }
+ return false
+ };
+ */
 /** /Функция, аналогичная php функции inArray **/
 
 
 /** Изменение строки даты из вида "2013-04-15" в вид "15.04.2013" **/
 /*
-function makeCalDate(date) {
-	var d = date.getDate().toString();
-	var m = 1 + date.getMonth();
-	var y = date.getFullYear().toString();
-	if (d.length < 2) {
-		d = "0" + d;
-	}
-	m = m.toString();
-	if (m.length < 2) {
-		m = "0" + m;
-	}
+ function makeCalDate(date) {
+ var d = date.getDate().toString();
+ var m = 1 + date.getMonth();
+ var y = date.getFullYear().toString();
+ if (d.length < 2) {
+ d = "0" + d;
+ }
+ m = m.toString();
+ if (m.length < 2) {
+ m = "0" + m;
+ }
 
-	var Date2 = d + "." + m + "." + y;
+ var Date2 = d + "." + m + "." + y;
 
-	return Date2;
-}
-*/
+ return Date2;
+ }
+ */
 /** /Изменение строки даты из вида "2013-04-15" в вид "15.04.2013" **/
