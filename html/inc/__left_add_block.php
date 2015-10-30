502cdc695a98d9117f5cd4ee64113e7f64892358
admin <master@salvo.ru>
2015-10-30 17:32:06 +0300

    Блоки DocumentBlock и DocCounterBlock вынесены в блок LeftAdditionalBlock

---
+++ html/inc/__left_add_block.php
@@ -0,0 +1,22 @@
+<!-- DocumentBlock -->
+<div class="doc-block">
+	<div class="caption-block">
+		Документы
+	</div>
+	0 из 0 документов <br/><br/>
+	готовы к формированию <br/><br/><br/>
+	<a href="#" class="button">Сформировать</a>
+
+</div>
+<!-- /DocumentBlock -->
+
+<!-- DocCounterBlock -->
+<div class="doc-counter-block">
+	<div class="center-text">
+		Счетчик
+	</div>
+	<br/><br/>
+	0 документов выгружено <br/><br/>
+	0 документов сформировано <br/><br/>
+</div>
+<!-- /DocCounterBlock -->
--- html/page-responsible-person.php
+++ html/page-responsible-person.php
@@ -40,33 +40,11 @@
 			</ul>
 			<!-- /LeftMenu -->
 
-			<!-- DocumentBlock -->
-			<br/><br/><br/>
-
-			<div class="doc-block">
-				<div class="caption-block">
-					Документы
-				</div>
-				<br/><br/>
-				0 из 0 документов <br/><br/>
-				готовы к формированию <br/><br/><br/>
-				<a href="#" class="button">Сформировать</a>
-
-			</div>
-			<!-- /DocumentBlock -->
-
-			<!-- DocCounterBlock -->
-			<br/><br/><br/>
-
-			<div class="doc-counter-block">
-				<div class="center-text">
-					Счетчик
-				</div>
-				<br/><br/>
-				0 документов выгружено <br/><br/>
-				0 документов сформировано <br/><br/>
-			</div>
-			<!-- /DocCounterBlock -->
+			<!-- LeftAdditionalBlock -->
+			<?
+				include('inc/__left_add_block.php');
+			?>
+			<!-- /LeftAdditionalBlock -->
 
 		</div>
 		<!-- /LeftColumn -->
